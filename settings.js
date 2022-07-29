c1e749ad4ae79a57e792da6f7aebdde52f53d4cb
Martenganteng <97916228+Martenganteng@users.noreply.github.com>
2022-07-29 09:46:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['628977491343','628977491343','628977491343'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['628977491343'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '628977491343' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'MartinTzy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Mart-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = '© Martin | BOT' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://youtube.com/channel/UCh6O1BW_q_DIydERc9eP-Fg'
+global.ig = 'https://instagram.com/'
+global.mygc = 'https://chat.whatsapp.com/KSQDAEZVxJS9azBjYumo7g'
+global.myweb = 'https://bit.ly/ASTABOT'
+global.email = 'martinlazuardi44@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Mart-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Martin' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -88,7 +88,7 @@ botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
 owner: '_Fitur Khusus Owner Bot_',
 group: '_Fitur Digunakan Hanya Untuk Group!_',
 private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
-bot: '_Fitur Khusus Pengguna Nomor Bot_',
+bot: '_Fitur Khusus Pengguna Nomor Botmk _',
 wait: '_Sedang Di Proses..._',
 error: '_Fitur Sedang Error!_',
 endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
